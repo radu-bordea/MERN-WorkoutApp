f3b9c4ba9d23cdb6c5d4f00a0853b49ca9608488
radu-bordea <radu.bordea.dev@gmail.com>
2025-04-03 13:13:41 +0200

    create workout schema and post workout route to database

---
+++ backend/models/workoutModel.js
@@ -0,0 +1,23 @@
+const mongoose = require("mongoose");
+
+const Schema = mongoose.Schema;
+
+const workoutSchema = new Schema(
+  {
+    title: {
+      type: String,
+      required: true,
+    },
+    reps: {
+      type: Number,
+      required: true,
+    },
+    load: {
+      type: Number,
+      required: true,
+    },
+  },
+  { timestamps: true } // Adds createdAt & updatedAt automatically
+);
+
+module.exports = mongoose.model("Workout", workoutSchema);
--- backend/routes/workouts.js
+++ backend/routes/workouts.js
@@ -1,30 +1,42 @@
-const express = require("express")
+const express = require("express");
+const Workout = require("../models/workoutModel");
 
-const router = express.Router()
+const router = express.Router();
 
 // GET all workouts
 router.get("/", (req, res) => {
-    res.json({mssg: "GET all workouts"})
-})
+  res.json({ mssg: "GET all workouts" });
+});
 
 // Get a single workout
 router.get("/:id", (req, res) => {
-    res.json({mssg: "GET a single workout"})
-})
+  res.json({ mssg: "GET a single workout" });
+});
 
 // POST a new workout
-router.post("/", (req, res) => {
-    res.json({mssg:"POST a new workout"})
-})
+router.post("/", async (req, res) => {
+  const { title, load, reps } = req.body;
+
+  try {
+    const workout = await Workout.create({
+      title,
+      load,
+      reps,
+    });
+    res.status(200).json(workout);
+  } catch (error) {
+    res.status(400).json({ error: error.message });
+  }
+});
 
 // DELETE a workout
 router.delete("/:id", (req, res) => {
-    res.json({mssg:"DELETE a workout"})
-})
+  res.json({ mssg: "DELETE a workout" });
+});
 
 // UPDATE new workout
 router.patch("/:id", (req, res) => {
-    res.json({mssg:"UPDATE a workout"})
-})
+  res.json({ mssg: "UPDATE a workout" });
+});
 
-module.exports = router
+module.exports = router;
