5f6faf8ae6f2797c385232df91d980490b3f2c65
radu-bordea <radu.bordea.dev@gmail.com>
2025-04-06 18:55:19 +0200

    Create WorkoutDetails component, and fetch workouts on Home Components and pass through props

--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -1,10 +1,34 @@
+import { useEffect, useState } from "react";
+
+// components
+import WorkoutDetails from "../components/WorkoutDetails";
 
 const Home = () => {
-  return (
-    <h1>
-        Home Page
-    </h1>
-  )
-}
+  const [workouts, setWorkouts] = useState();
 
-export default Home
+  useEffect(() => {
+    const fetchWorkouts = async () => {
+      const response = await fetch("/api/workouts");
+      const json = await response.json();
+
+      if (response.ok) {
+        setWorkouts(json);
+      }
+    };
+
+    fetchWorkouts();
+  }, []);
+
+  return (
+    <div className="home">
+      <div className="workouts">
+        {workouts &&
+          workouts.map((workout) => (
+            <WorkoutDetails key={workout._id} workout={workout} />
+          ))}
+      </div>
+    </div>
+  );
+};
+
+export default Home;
